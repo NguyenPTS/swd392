9f79d9ed1fa9ff7ee01791b7ad64c1c8bafb7fda
huybng <noname210603@gmail.com>
2024-01-30 08:11:48 +0700

    push new codes

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Account from './components/Account';
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <div>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
+}
+
+export default App;
